3a573cbfa50c3d055572d2e6d00a3572a3e4a094
nescatfe <fajarsnick@gmail.com>
2023-02-28 11:15:06 +0700

    crypto sec

---
+++ crypto/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRYPTOSEC</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>CRYPTOSEC</h1>
+	</header>
+	<main>
+		<form method="POST">
+			<label for="token_address">Enter token address:</label>
+			<input type="text" name="token_address" id="token_address" required>
+			<button type="submit" name="moonarch">moonarch.app</button>
+			<button type="submit" name="tokensniffer">tokensniffer.com</button>
+			<button type="submit" name="gopluslabs">gopluslabs.io</button>
+		</form>
+	</main>
+	<footer>
+		<p>&copy; 2023 CRYPTOSEC</p>
+	</footer>
+</body>
+</html>
