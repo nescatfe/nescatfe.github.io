94b59518454a96d45e0815f3f199ca0d79c3d810
nescatfe <fajarsnick@gmail.com>
2023-03-01 10:45:10 +0700

    default

--- front/script.js
+++ front/script.js
@@ -10,48 +10,25 @@ const statusContainer = document.querySelector('.status-container');
 const dot = document.querySelector('.dot');
 
 // Set the default status text and dot class
-let statusText = 'Busy';
+let statusText = 'Busy\u00A0';
 let dotClass = 'red-dot';
 
 // Determine the status and dot class based on the hour in GMT+7
 if (hour >= 8 && hour < 17) {
-  statusText = 'Busy';
+  statusText = 'Busy\u00A0';
   dotClass = 'red-dot';
 } else if (hour >= 18 && hour < 21) {
-  statusText = 'Free Time';
+  statusText = 'Free Time\u00A0';
   dotClass = 'green-dot';
 } else if (hour >= 22 || hour < 2) {
-  statusText = 'Resting';
+  statusText = 'Resting\u00A0';
   dotClass = 'silver-dot';
 }
 
-// Set the initial text and dot class in the status container
+// Set the text and dot class in the status container
 document.getElementById('status-text').textContent = statusText;
 dot.classList.add(dotClass);
 
 // Set the time in the status container
 const timeText = now.toLocaleTimeString([], { timeZone: 'GMT', hour12: false, hour: '2-digit', minute: '2-digit' });
 document.getElementById('time').textContent = `- Working | ${timeText} GMT+7 `;
-
-// Listen for changes to the status text and dot class from the controller page
-window.addEventListener('message', (event) => {
-  // Only allow messages from the same origin
-  if (event.origin !== window.location.origin) {
-    return;
-  }
-
-  const { type, status, dot } = event.data;
-
-  if (type === 'updateStatus') {
-    // Update the status text
-    statusText = status;
-    document.getElementById('status-text').textContent = statusText;
-  } else if (type === 'updateDot') {
-    // Remove any existing dot class
-    dot.classList.remove(dotClass);
-
-    // Update the dot class
-    dotClass = dot;
-    dot.classList.add(dotClass);
-  }
-});
