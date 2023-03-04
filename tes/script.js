7854ae0f2cb45bc709ba0b1a239ba89825cc061f
nescatfe <fajarsnick@gmail.com>
2023-03-04 21:07:06 +0700

    delete

--- tes/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function() {
-  var header = document.querySelector("header");
-  var content = document.querySelector(".content");
-  if (window.pageYOffset > header.offsetHeight) {
-    content.classList.add("blur");
-  } else {
-    content.classList.remove("blur");
-  }
-});
